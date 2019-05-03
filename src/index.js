752b2c89eaeaad9cf35cda7f1d2b8a90c8198c3a
Ismael Barragán <ismael@mabkil.com>
2019-05-03 15:11:11 -0500

    Basic bindings

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import Vue from 'vue';
 
+const todos = [
+  { text: '0' },
+  { text: '1' },
+  { text: '2' },
+];
+
 const App = new Vue({
   el: '#vue-playground',
   data: {
-    message: 'Hello Vue World!'
+    message: 'Hello Vue World!',
+    seen: true,
+    todos: todos,
+    binding: '',
   }
 });
+
+function toggleVisible() {
+  App.seen = !App.seen;
+}
+
+function addTodo() {
+  todos.push({ text: todos.length.toString() });
+}
+
+const button = document.getElementById('testButton');
+button.addEventListener('click', toggleVisible);
+
+const button2 = document.getElementById('testButton2');
+button2.addEventListener('click', addTodo);
+
+window.vueApp= App;
